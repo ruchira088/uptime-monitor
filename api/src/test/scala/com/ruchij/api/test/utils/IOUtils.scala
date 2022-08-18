dab9ae44f10bc3c6d32aadc851314981812b34e3
Ruchira (Richie) Jayasekara <me@ruchij.com>
2022-08-18 19:09:20 +1000

    Updating to Scala 3 syntax

--- api/src/test/scala/com/ruchij/api/test/utils/IOUtils.scala
+++ api/src/test/scala/com/ruchij/api/test/utils/IOUtils.scala
@@ -2,17 +2,17 @@ package com.ruchij.api.test.utils
 
 import cats.effect.IO
 import cats.effect.unsafe.implicits.global
+import org.checkerframework.checker.units.qual.h
 
 object IOUtils {
 
   def runIO[A](block: => IO[A]): A = block.unsafeRunSync()
 
-  implicit class IOErrorOps(value: IO[_]) {
-    val error: IO[Throwable] =
-      value.attempt.flatMap {
+  extension [A] (io: IO[A]) {
+    def error: IO[Throwable] =
+      io.attempt.flatMap {
         case Left(throwable) => IO.pure(throwable)
         case Right(success) => IO.raiseError(IllegalStateException(s"Expected an exception, but returned $success"))
       }
   }
-
 }
